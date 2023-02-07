16addc6c0cba018dff35e69eff1b0d30b53254ef
sandeep kiran <sandeepkiranp@gmail.com>
2023-02-07 07:56:22 -0500

    exception handling

--- hw1/findpriv.py
+++ hw1/findpriv.py
@@ -11,25 +11,30 @@ capabilities_files = {}
 def walktree(top, setuid, capabilities):
     '''recursively descend the directory tree rooted at top'''
     global filecount, execcount, setuid_files, capabilities_files
-    for f in os.listdir(top):
-        pathname = os.path.join(top, f)
-        mode = os.lstat(pathname).st_mode
-        if S_ISDIR(mode):
-            # It's a directory, recurse into it
-            walktree(pathname, setuid, capabilities)
-        else:
-            if os.access(pathname, os.X_OK):
-                execcount +=1
-                if setuid and (mode & stat.S_ISUID):
-                    setuid_files.append(pathname)
-                if capabilities:
-                    import subprocess
-                    result = subprocess.run(['getcap', pathname], stdout=subprocess.PIPE)
-                    if result.stdout.decode('ascii'):
-                        output = result.stdout.decode('ascii')
-                        print('append to the capabilities list if capabilities is set' + pathname + ' ' + output)
-                        capabilities_files[output.split()[0]] = output.split()[1].split('=')[0]
-            filecount +=1
+    try:
+        for f in os.listdir(top):
+            try:
+                pathname = os.path.join(top, f)
+                mode = os.lstat(pathname).st_mode
+                if S_ISDIR(mode):
+                    # It's a directory, recurse into it
+                    walktree(pathname, setuid, capabilities)
+                else:
+                    if os.access(pathname, os.X_OK):
+                        execcount +=1
+                        if setuid and (mode & stat.S_ISUID):
+                            setuid_files.append(pathname)
+                        if capabilities:
+                            import subprocess
+                            result = subprocess.run(['getcap', pathname], stdout=subprocess.PIPE)
+                            if result.stdout.decode('ascii'):
+                                output = result.stdout.decode('ascii')
+                                capabilities_files[output.split()[0]] = output.split()[1].split('=')[0]
+                    filecount +=1
+            except:
+                pass
+    except:
+        pass
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", action="store_true", dest="setuid")
@@ -38,7 +43,7 @@ parser.add_argument("-p", default="/",dest="path")
 
 options = parser.parse_args()
 if not len(sys.argv) > 1:
-    print('No arguments passed')
+    #print('No arguments passed')
     options.setuid = True
     options.capabilities = True
     options.path = "/"
@@ -47,16 +52,16 @@ if not options.setuid and not options.capabilities:
     options.setuid = True
     options.capabilities = True
 
-print ('Argument setuid:', options.setuid)
-print ('Argument capabilities:', options.capabilities)
-print ('Argument path:', options.path)
+#print ('Argument setuid:', options.setuid)
+#print ('Argument capabilities:', options.capabilities)
+#print ('Argument path:', options.path)
 
 walktree(options.path, options.setuid, options.capabilities)
 print('Scanned ' + str(filecount) + ' files, found ' + str(execcount) + ' executables')
-print('setuid executables:')
+options.setuid and print('setuid executables:')
 for x in setuid_files:
-    print(x + '\n')
-print('capability-aware executables:')
+    print(x)
+options.capabilities and print('capability-aware executables:')
 for x in capabilities_files.keys():
-    print(x + '\t' + capabilities_files[x] + '\n')
+    print(x + '\t' + capabilities_files[x])
 
